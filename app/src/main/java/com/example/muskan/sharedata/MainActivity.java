96231fea6198bce0859c025bc8a2df7c95b08926
muskan2200 <goyalmuskan288@gmail.com>
2016-08-16 16:56:48 +0530

    new code added

---
+++ app/src/main/java/com/example/muskan/sharedata/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.muskan.sharedata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText message;
+
+    Button share;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message=(EditText)findViewById(R.id.input);
+
+        share=(Button)findViewById(R.id.sharemessage);
+        share.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+        Intent shareintent=new Intent();
+        shareintent.setAction(Intent.ACTION_SEND);
+        shareintent.putExtra(Intent.EXTRA_TEXT,message.getText().toString());
+        shareintent.setType("text/plain");
+        startActivity(Intent.createChooser(shareintent,"share title "));    }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
